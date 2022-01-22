b66232954ae434fce7f0ba22a6888059035c8f75
Freddie Yeo <63839465+fjyeo@users.noreply.github.com>
2022-01-22 19:44:25 +0000

    seaside and jungle update

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,10 @@
 import string
 
 
-introduction = "Hello, welcome to the adventure survival game. During the game you will make several desicions and answer questions which will determine the outcome of your advenuture"
+ = "Hello, welcome to the adventure survival game. During the game you will make several desicions and answer questions which will determine the outcome of your advenuture"
 instruction = "You will now get to decide what youre character will be, please enter youre gender: male or female"
 print(introduction + instruction)
-gender = input().lower()
+gender = input().lower()                                                                                     
 
 if gender == "male":
     print("You have picked male")
@@ -26,8 +26,34 @@ if biome_choice == "jungle":
 
         if shelter_or_way_out == "way out":
             print("You have decided to go up the mountain to try find a way out")
+            print("You make the trek up the mountain and then see endless jungle but see a nice patch of open land about 1km away.Please pick your option:\n walk or try find shelter")
+            walk_or_shelter = input().lower()
+            
+            if "walk" in walk_or_shelter == "walk to the open land":
+                print("You have decided to walk over to the patch of land so you start your walk")
+                print("After an hou you made your way to the open patch of land and find out that it is perfect to setup a place to live")
+                print("You have many natural resources around you so you start gathering up trees and vines and make a quick simple structure to cover yourself from rain")
+                print("It becomes night and you didnt geg hit by the rain but your legs had mosquito bites on them and figured out that you would have to find a way to not get bitten"
+            
         else:
             print("You have decided to carry on youre objective to find shelter")
+            print("You find an overhang where you would be protected from the rain.")
+            print("it becomes night and you get a good sleep but your back was aching from sleeping on stone")
+            print("You see some bugs on the floor, they are nutritious but could have bacteria on them. Do you eat them or not Yes or No?")
+            yes_or_no_jungle_worms = input().lower()
+            
+            if "Yes" in yes_or_no_jungle_worms == "Eating worms":
+                      print("You have decided to eat the jungle worms")
+                      print("The worms tasted horrible and chewy but gave you enough energy for the day but you need water before the end of the day")
+                      print("It starts raining. You can either collect the rain water or try find a fresh water source which could be miles.")
+                      print("Please pick your option:Collect water or find Water source")
+                      collect_rain_water_or_find_water_source = input().lower()
+                      
+                      if "Rain" in collect_rain_water_of_find_water_source == "collecting rain water":
+                                print("You have decided to collect the rain water")
+                      else:
+                           print("You have decided to try find a water source")
+                    
     else:
         print("You have picked to find food and water")
         print("You make youre way through the vegitation and then see in the distance a great waterfall....But there are tigers in the way. You could try sneek past the tigers or go around them. Please pick your option: \n Sneek or Go around")
@@ -35,9 +61,21 @@ if biome_choice == "jungle":
 
         if sneek_or_go_around_jungle == "sneek":
             print("You have decided to try sneek around the tigers")
+            print("You get close to the waterful. But the tigers were hungryand needed some food to live")
+            print("They quickly jump at you pinning you to the ground and start eating your body eventually killing you. Unfortunatly you have died due to your desicions")
+            exit()
+            
+            
         else:
-            print("You decided to go around the tigers")    
-    
+            print("You decided to go around the tigers")
+            print("it took awhile to cut through the vegitation but after awhile you manage to get to the water source and hydrate again")
+            print("You are also very dirty but there could be dangerous animals in the water. Should you jump in and wash up. Yes or No")
+            wash_up_or_not_jungle = input().lower()
+            
+            if "Yes" in wash_up_or_not_jungle == "Yes":
+                      print:("There was viscious piranhas in the pool. You tried to escape but there was too many of them. You ended up dying because of your choices.")
+                      break()
+   
 
 if biome_choice == "desert":
     print("You have picked the Desert biome. This biome is a challenge to survive in and has little food and water ")
@@ -99,7 +137,46 @@ if biome_choice == "Grass land":
     
 
 if biome_choice == "Coast line":
-    print ("You have picked the coast line biome. This biome has ood food and water but you are at risk of storms") 
-    print ("You look around there are clips and rocks around you and you feel the sof touch of sand beneath your feet")
+    print("You have picked the coast line biome. This biome has ood food and water but you are at risk of storms") 
+    print("You look around there are clips and rocks around you and you feel the sof touch of sand beneath your feet")
+    print("You suddenly tralise that you have no food, water or shelter")
+    print("You have gwo options you could either try find a shelter or food and water. Please pick your option now:\n Food and water or shelter")
+    food_and_water_or_shelter_seaside input().lower()
+    
+    if "food" in food_and_water_or_shelter_seaside or "water" in food_and_water_or_shelter_seaside == "food and water":
+        print("You have chosen to try find food and water ")
+        print("You start looking for food and come to the desicion that the best way to get food is to catch fish but theres two different options. Please pick your option:\n Use hands or Make weapon")
+        weapon_or_hands input().lower()
+       
+        if "weapon" in weapon_or_hands == "weapon"
+            print("You have chosen to try make a weapon to try catch some fish")
+            print("You find a sharp stone and a sharp stick. You could use either of these items to catch the fish.Please pick your option:\n Stone or Wood")
+            stone_or_wood input().lower()
+            
+            if "stone" in stone_or_wood == "stone":
+                print("You have chosen to use the sharp stone to try catch fish which is heavier to carry but more dangerous then the stick")
+                print("You used the stone but couldn't move quickly so you only caught a few fish")
+                print("You had to sleep on the sand but got a reasonable seat as you had a bit of food")
+            else:
+                print("You have chosen to use the sharp wood which is lighter then the stone but not as dangerous")
+                print("You managed to catch lots of fishas you could move quickly through the water and catch the fish")
+                print("You had to sleep on the sand but got a good sleep as you caught a lot of fish")
+        else:
+            print("You have chosen to try use your hands to catch the fish")
+            print("You ran into the sea. There was several fish but they were to quick for you so you gave up when it came dark.")
+            print("You ended up having to sleep on the sand and didnt get a good sleep")
+    else:
+        print("You have chosen to try find shelter")
+        print("Where will you start looking for shelter. There is the remains of an old boat or theres a cave. Pick your option now:\n Boat or Cave")
+        boat_or_cave input().lower()
+        
+        if "boat" in boat_or_cave == "boat":
+            print("You have chosen to go look for shelter at the abandoned boat")
+            print("You walk over to the Abandoned boat and go outside. Its nicely shaded and has good protection from the wind")
+            print("It turns into night and you get a good sleep and are ready for the day ahead")
+        else:
+            print("You have decided to look for shelter in the cave")
+            print("You go into the cave and everything seems great. Its nicely protected from the wind and cool.")
+            print("Day turns into night and you get to sleep well but....\n you wake up and your body is covered in bites and you are covered in sweat. You try to get up but your legs are aking horribly. Thi         is puts you in a disadvantage for the next day")
 
 
